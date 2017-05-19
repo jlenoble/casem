e6bc9f1f0206775ee9647fc03bfd3d6767a4b7b7
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-19 02:07:56 +0200

    Refactored Getkey test with makeIOTest
    The factory was especially developed within package child-process-data
    to streamline testing in casem.

--- test/casem.test.js
+++ test/casem.test.js
@@ -1,54 +1,16 @@
-import Muter, {captured} from 'muter';
-import {expect} from 'chai';
-import {spawn} from 'child_process';
-import childProcessData, {makeSingleTest} from 'child-process-data';
+import {makeIOTest} from 'child-process-data';
 
 describe('Testing Casem', function () {
   it('Testing Getkey loop', function () {
-    const test = makeSingleTest({
-      debug: true,
-      spawnTest () {
-        const p = spawn('node', [
-          'build/src/casem.js',
-          'src/static/data/getkey.txt',
-        ], {stdio: 'pipe'});
+    const test = makeIOTest({
+      childProcessFile: 'build/src/casem.js',
+      childProcessOptions: ['src/static/data/getkey.txt'],
+      waitForReady: 1000,
 
-        this.childProcess = p;
-
-        childProcessData(p);
-      },
-      checkResults () {
-        const muter = new Muter(process);
-
-        const doCheck = () => {
-          const logs = muter.getLogs();
-
-          try {
-            expect(logs).to.match(/Entering loop\n/);
-          } catch (err) {
-            return new Promise(resolve => {
-              setTimeout(() => {
-                resolve(doCheck());
-              }, 10);
-            });
-          }
-
-          this.childProcess.stdin.write('Hello\r');
-
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              try {
-                expect(muter.getLogs()).to.match(/Leaving loop\n/);
-                resolve();
-              } catch (err) {
-                reject(err);
-              }
-            }, 10);
-          });
-        };
-
-        return captured(muter, doCheck)();
-      },
+      messages: [
+        {o: 'Entering loop\n'},
+        {io: ['Hello', 'Leaving loop\nGood bye!\n']},
+      ],
     });
 
     return test();
