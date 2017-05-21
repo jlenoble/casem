dccf84ed5e9da0996740d7c2cb3c7202e19edea3
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-21 15:34:31 +0200

    Factored out PrintStats

--- src/Stats.js
+++ src/Stats.js
@@ -1,25 +1,4 @@
-export const mixWithPrintStats = Interpreter => {
-  Object.assign(Interpreter.prototype, {
-    visitPrint (ctx) {
-      let txt = ctx.STRING().getText().replace(/^"(.*)"$/, '$1');
-      this.screen.print(txt);
-    },
-
-    visitPrintAt (ctx) {
-      const x = this.visit(ctx.numExpr(0));
-      const y = this.visit(ctx.numExpr(1));
-      let txt;
-
-      if (ctx.STRING() !== null) {
-        txt = ctx.STRING().getText().replace(/^"(.*)"$/, '$1');
-      } else {
-        txt = this.visit(ctx.numExpr(2)).toString();
-      }
-
-      this.screen.printAt(x, y, txt);
-    },
-  });
-};
+import {mixWithPrintStats} from './PrintStats';
 
 export const mixWithStats = Interpreter => {
   Object.assign(Interpreter.prototype, {
