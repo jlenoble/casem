50eead1ccc69becb9a778a139318fa7fb2ae193e
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-22 19:29:16 +0200

    File extends Block
    Moved all logic regarding queue to higher abstraction Block
    Files will be used to encapsulate jumps, but queues are relative to
    blocks (as in Files, but also Loops).

--- src/file.js
+++ src/file.js
@@ -1,35 +1,8 @@
-import Queue from './queue';
+import Block from './block';
 
-class File {
+class File extends Block {
   constructor (filename, visitor) {
-    Object.defineProperties(this, {
-      queue: {
-        value: new Queue(),
-      },
-      visitor: {
-        value: visitor,
-      },
-    });
-  }
-
-  doQueue (func) {
-    return this.queue.doQueue(func);
-  }
-
-  execStat (ctx) {
-    this.visitor.visitChildren(ctx);
-  }
-
-  flush (resolve) {
-    return this.queue.flush(resolve);
-  }
-
-  queueStat (ctx) {
-    if (this.queue.length) {
-      this.queue.doQueue(() => this.execStat(ctx));
-    } else {
-      this.execStat(ctx);
-    }
+    super(visitor);
   }
 }
 
