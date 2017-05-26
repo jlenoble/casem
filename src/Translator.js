43f37abbfc9091025407653806fe44f142efdc5e
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-26 09:42:13 +0200

    Moved Translator to top src dir

--- src/Translator.js
+++ src/Translator.js
@@ -2,9 +2,10 @@ import path from 'path';
 
 const base = process.cwd();
 const rel = path.relative(base, 'src/static/antlr4/parsers');
-const {CalcListener} = require(path.join(base, rel,
-  'CalcListener'));
+const {CalcListener} = require(path.join(base, rel, 'CalcListener'));
 
 export class Translator extends CalcListener {
-  enterAdditiveExpr (ctx) {}
+  enterProg (ctx) {}
+
+  exitProg (ctx) {}
 }
